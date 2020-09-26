cce38113c52ee9caf5e1cb3ab0a8dd1f3585ac4d
ganyuanrong <372129081@qq.com>
2020-09-26 17:58:04 +0800

    修改SDK登录 注册上报

--- GamaSDK_iOS_Integration/FLSDK/login/SDKLoginViewController.m
+++ GamaSDK_iOS_Integration/FLSDK/login/SDKLoginViewController.m
@@ -376,15 +376,23 @@
         //是否需要保存账号密码
         if (SDK_DATA.mCCSDKResponse.account && SDK_DATA.mCCSDKResponse.password) {
             if (SDK_DATA.mCCSDKResponse.code == 1001) {//注册
-                [GamaAdInterface allEventReportWithEventName:GAMESWORD_EVENT_REGISTER parameters:@{@"userId":cc.userId}];
+//                [GamaAdInterface allEventReportWithEventName:GAMESWORD_EVENT_REGISTER parameters:@{@"userId":cc.userId}];
                 [[ConfigCoreUtil share] saveAccount:SDK_DATA.mCCSDKResponse.account password:SDK_DATA.mCCSDKResponse.password updateTime:YES];
             }else if([ConfigCoreUtil share].isSaveAccountInfo){//登录并勾选记住密码
-                [GamaAdInterface allEventReportWithEventName:GAMESWORD_EVENT_LOGIN parameters:@{@"userId":cc.userId}];
+//                [GamaAdInterface allEventReportWithEventName:GAMESWORD_EVENT_LOGIN parameters:@{@"userId":cc.userId}];
                 [[ConfigCoreUtil share] saveAccount:SDK_DATA.mCCSDKResponse.account password:SDK_DATA.mCCSDKResponse.password updateTime:YES];
             }
             
         }
 
+    }
+    
+    if (SDK_DATA.mCCSDKResponse.code == 1001) {//注册
+        [GamaAdInterface allEventReportWithEventName:GAMESWORD_EVENT_REGISTER parameters:@{@"userId":cc.userId}];
+        
+    }else {//登录
+        [GamaAdInterface allEventReportWithEventName:GAMESWORD_EVENT_LOGIN parameters:@{@"userId":cc.userId}];
+        
     }
     
     if ([FLSDK share].loginCompletionHandler) {
