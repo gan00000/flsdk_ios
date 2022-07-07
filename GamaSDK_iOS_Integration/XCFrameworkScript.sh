21d7e5fea1951c39f363cd43396af6810b6bacf7
Gan Yuanrong <372129081@qq.com>
2022-07-07 14:24:35 +0800

    添加注释

--- GamaSDK_iOS_Integration/XCFrameworkScript.sh
+++ GamaSDK_iOS_Integration/XCFrameworkScript.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # 放在与 .xcodeproj 文件同级目录下，生成结果在 build 目录下
+#取当前运行脚本的所在路径：$0
+#取当前脚本所在路径的父目录：dirname "$0"
+#取返回的父目录的值：$(dirname "$0")
+#cd到返回的父目录：cd "$(dirname "$0")"
+#输出地址：cd "$(dirname "$0")";pwd
+#取输出的地址，并赋值给BIN_FOLDER：BIN_FOLDER = $(cd "$(dirname "$0")";pwd)
 
 # 需要编译的 scheme
 scheme="MWSDK"
