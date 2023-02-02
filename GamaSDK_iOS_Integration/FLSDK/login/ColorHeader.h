84745f5a0cbe7ec9d7b167b90818e0405f72295d
Gan Yuanrong <372129081@qq.com>
2023-02-02 14:36:37 +0800

    去掉多余的target

--- GamaSDK_iOS_Integration/FLSDK/login/ColorHeader.h
+++ GamaSDK_iOS_Integration/FLSDK/login/ColorHeader.h
@@ -2,28 +2,8 @@
 #ifndef ColorHeader_h
 #define ColorHeader_h
 
-#ifdef SDK_V2
-
-#define BaseColor  wwwww_tag_wwwww__CC_FF892E
-#define C_BUTTON_START  wwwww_tag_wwwww__CC_4CADFE
-#define C_BUTTON_END  wwwww_tag_wwwww__CC_02EDFC
-
-#elif SDK_V3
-
-#define BaseColor  wwwww_tag_wwwww__CC_4CADFE
-#define C_BUTTON_START  wwwww_tag_wwwww__CC_4CADFE
-#define C_BUTTON_END  wwwww_tag_wwwww__CC_02EDFC
-
-#elif SDK_V4
-
-#define BaseColor  wwwww_tag_wwwww__CC_03CAFC
-#define C_BUTTON_START  wwwww_tag_wwwww__CC_90FD9E
-#define C_BUTTON_END  wwwww_tag_wwwww__CC_00C9FE
-
-#else
-
-#define BaseColor  wwwww_tag_wwwww__CC_FF892E
-
-#endif
+#define BaseColor  wwwww_tag_wwwww__CC_FC676F
+#define C_BUTTON_START  wwwww_tag_wwwww__CC_FC676F
+#define C_BUTTON_END  wwwww_tag_wwwww__CC_FF9944
 
 #endif  
