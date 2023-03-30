1af1ab286d0d2829e89b8c00970b0856a5560b2d
Gan Yuanrong <372129081@qq.com>
2023-03-30 21:35:44 +0800

    vn 更改图片

--- GamaSDK_iOS_Integration/FLSDK/login/view_vn/widget/SDKTextFiledView.m
+++ GamaSDK_iOS_Integration/FLSDK/login/view_vn/widget/SDKTextFiledView.m
@@ -26,7 +26,7 @@
             
             [self addContentView_v2_MMMethodMMM:type];
         }else{
-            [self addContentView_MMMethodMMM:type];
+            
         }
         
     }
@@ -55,197 +55,6 @@
     
 }
 
-
-- (void) addContentView_MMMethodMMM:(SDKTextFiledView_Type) type
-{
-    
-    //    self.backgroundColor = [UIColor whiteColor];
-    //    self.layer.cornerRadius = 4;
-    
-    NSString *iconName;
-    NSString *lableName;
-    BOOL showEye = NO;
-    BOOL addMoreAccountBtn = NO;
-    UIKeyboardType mUIKeyboardType = UIKeyboardTypeDefault;
-    NSString *placeholderText = @"";
-    BOOL showLableIcon = YES;
-    
-    switch (type) {
-        case SDKTextFiledView_Type_VfCode:
-            iconName = wwwww_tag_wwwww_fl_sdk_dx_png;
-            lableName = wwwww_tag_wwwww_soonoon_teleoator;
-            mUIKeyboardType = UIKeyboardTypeNumberPad;
-            placeholderText = wwwww_tag_wwwww_corticoaster_numberature;
-            showLableIcon = NO;
-            break;
-            
-        case SDKTextFiledView_Type_Account:
-            iconName = mw_account_icon;
-            lableName = wwwww_tag_wwwww_broncheer_whitester;
-            placeholderText = wwwww_tag_wwwww_oursure_pleging;
-            addMoreAccountBtn = YES;
-            break;
-            
-        case SDKTextFiledView_Type_Password:
-            
-            iconName = mw_passowrd_icon;
-            lableName = wwwww_tag_wwwww_almostive_penotic;
-            placeholderText = @"請輸入6-20字元";
-            showEye = YES;
-            break;
-            
-        case SDKTextFiledView_Type_Password_Again:
-            
-            iconName = mw_passowrd_icon;
-            lableName = wwwww_tag_wwwww_almostive_penotic;
-            placeholderText = wwwww_tag_wwwww_zyment_gamid;
-            showEye = YES;
-            break;
-            
-        case SDKTextFiledView_Type_Password_New:
-            iconName = mw_passowrd_icon;
-            lableName = wwwww_tag_wwwww_scientist_misceair;
-            placeholderText = wwwww_tag_wwwww_scientist_misceair;//GetString(wwwww_tag_wwwww_TXT_PH_ACCOUNT_INPUT_PWD_NEW);
-            showEye = YES;
-            break;
-        case SDKTextFiledView_Type_Password_Old:
-            iconName = mw_passowrd_icon;
-            lableName = wwwww_tag_wwwww_fontmentionry_labor;
-            placeholderText = wwwww_tag_wwwww_fontmentionry_labor;//GetString(wwwww_tag_wwwww_TXT_PH_ACCOUNT_INPUT_PWD_OLD);
-            showEye = YES;
-            break;
-            
-        default:
-            break;
-    }
-    
-    self.lableIconImageView = [[UIImageView alloc] initWithImage:[UIImage res_imageNamed_MMMethodMMM:iconName]];
-    self.lableIconImageView.contentMode = UIViewContentModeScaleAspectFit;
-    [self addSubview:self.lableIconImageView];
-    [self.lableIconImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.leading.equalTo(self.mas_leading);
-        make.centerY.mas_equalTo(self);
-        make.height.mas_equalTo(VH(21));//icon,字体需要使用高度比
-        make.width.mas_equalTo(self.lableIconImageView.mas_height);
-    }];
-    
-    
-    UIView *lineView1 = [[UIView alloc] init];
-    lineView1.backgroundColor = [UIColor colorWithHexString_MMMethodMMM:wwwww_tag_wwwww__CC_C0C0C0];
-    [self addSubview:lineView1];
-    [lineView1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        
-        make.width.mas_equalTo(0.5);
-        make.leading.mas_equalTo(self.lableIconImageView.mas_trailing).mas_offset(VW(8));
-        make.centerY.mas_equalTo(self);
-        
-        make.height.mas_equalTo(VH(14));
-        
-    }];
-    
-    
-    int trailing_offset = -10;
-    if (addMoreAccountBtn || showEye) {
-        trailing_offset = -VW(30);
-    }
-    
-    mUITextField = [[UITextField alloc] init];
-    self.inputUITextField = mUITextField;
-    if (mUIKeyboardType) {
-        [mUITextField setKeyboardType:mUIKeyboardType];
-    }
-    mUITextField.textColor = [UIColor whiteColor];
-    
-    mUITextField.font = [UIFont systemFontOfSize:FS(14)];
-    
-    if (SDK_DATA.mUITextFieldDelegate) {
-        mUITextField.delegate = SDK_DATA.mUITextFieldDelegate;
-    }
-    
-//    mUITextField.adjustsFontSizeToFitWidth = YES;//文字大小适配宽度大小n
-    
-    // 设置placeholder文字大小和居中显示
-    //    NSMutableParagraphStyle *style = [mUITextField.defaultTextAttributes[NSParagraphStyleAttributeName] mutableCopy];
-    //    style.minimumLineHeight = mUITextField.font.lineHeight - (mUITextField.font.lineHeight - [UIFont systemFontOfSize:14.0].lineHeight) / 2.0;
-    
-    int placeHolderTextSize = FS(14);
-    NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:placeholderText
-                                                                  attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:placeHolderTextSize],NSForegroundColorAttributeName: [UIColor colorWithHexString_MMMethodMMM:wwwww_tag_wwwww__CC_C0C0C0]}];
-    mUITextField.attributedPlaceholder = attrStr;
-    //mUITextField.placeholder = placeholderText;
-    
-    [self addSubview:mUITextField];
-    [mUITextField mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.leading.equalTo(lineView1.mas_trailing).mas_offset(VW(8));
-        make.top.mas_equalTo(self).offset(2);
-        make.bottom.mas_equalTo(self).offset(-2);
-        make.trailing.mas_equalTo(self.mas_trailing).mas_offset(trailing_offset);
-        
-    }];
-    
-    if (addMoreAccountBtn) {
-        
-        self.moreAccountBtn = [UIUtil initBtnWithNormalImage_MMMethodMMM:sdk_list_down highlightedImage_MMMethodMMM:sdk_list_down selectedImageName_MMMethodMMM:sdk_list_up tag_MMMethodMMM:kMoreAccountListActTag selector:@selector(clickItemBtn_MMMethodMMM:) target_MMMethodMMM:self];
-        //        self.moreAccountBtn.hidden = YES;
-        self.moreAccountBtn.imageView.contentMode = UIViewContentModeScaleAspectFit;
-        [self addSubview:self.moreAccountBtn];
-        [self.moreAccountBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-            
-            make.trailing.mas_equalTo(self).mas_offset(VW(-8));;
-            make.centerY.mas_equalTo(self);
-            
-            make.height.mas_equalTo(self.mas_height);
-            make.width.mas_equalTo(VW(17));
-        }];
-        
-    }
-    
-    
-    
-    if (showEye) {
-        mUITextField.secureTextEntry = NO;
-        eyeBtn = [UIUtil initBtnWithNormalImage_MMMethodMMM:fl_sdk_ky highlightedImage_MMMethodMMM:fl_sdk_ky selectedImageName_MMMethodMMM:fl_sdk_by tag_MMMethodMMM:22 selector:@selector(eyeViewBtnAction_MMMethodMMM:) target_MMMethodMMM:self];
-        eyeBtn.selected = YES;//设置为选择
-        mUITextField.secureTextEntry = YES;
-        
-        eyeBtn.imageView.contentMode = UIViewContentModeScaleAspectFit;
-        [self addSubview:eyeBtn];
-        [eyeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-            
-            make.trailing.mas_equalTo(self).mas_offset(VW(-8));
-            make.centerY.mas_equalTo(self);
-            
-            make.height.mas_equalTo(self.mas_height);
-            make.width.mas_equalTo(VW(17));
-           
-        }];
-    }
-    
-    
-    UIView *lineView2 = [[UIView alloc] init];
-    lineView2.backgroundColor = [UIColor colorWithHexString_MMMethodMMM:wwwww_tag_wwwww__CC_C0C0C0];
-    [self addSubview:lineView2];
-    [lineView2 mas_makeConstraints:^(MASConstraintMaker *make) {
-        
-        //make.width.mas_equalTo(0.5);
-        make.leading.trailing.mas_equalTo(self);
-        make.bottom.mas_equalTo(self);
-        
-        make.height.mas_equalTo(1);
-        
-    }];
-    
-    if (!showLableIcon) {
-        self.lableIconImageView.hidden = YES;
-        lineView1.hidden = YES;
-        lineView2.hidden = YES;
-    }
-    
-    if (self.inputUITextField) {
-        [self.inputUITextField addTarget:self action:@selector(textChanged_MMMethodMMM:) forControlEvents:(UIControlEventEditingChanged)];
-    }
-    
-}
 
 - (void) addContentView_v2_MMMethodMMM:(SDKTextFiledView_Type) type
 {
@@ -347,8 +156,8 @@
     if (mUIKeyboardType) {
         [mUITextField setKeyboardType:mUIKeyboardType];
     }
-    mUITextField.textColor = [UIColor whiteColor];
-    
+    mUITextField.textColor = [UIColor colorWithHexString_MMMethodMMM:C_TEXT_NORMAL_2];
+
     mUITextField.font = [UIFont systemFontOfSize:FS(14)];
     
     if (SDK_DATA.mUITextFieldDelegate) {
