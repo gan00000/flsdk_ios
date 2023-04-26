b26c952676f1edb964965e1f804cda9dff704d76
Gan Yuanrong <372129081@qq.com>
2023-04-26 16:58:09 +0800

    v6-宏定义区分

--- GamaSDK_iOS_Integration/FLSDK/login/ColorHeader.h
+++ GamaSDK_iOS_Integration/FLSDK/login/ColorHeader.h
@@ -42,6 +42,12 @@
 #define C_TEXT_NORMAL_2   @"#414141"
 #define C_TEXT_HINT    @"#C0C0C0"
 
+#elif SDK_V6
+
+#define BaseColor  wwwww_tag_wwwww__CC_FC676F
+#define C_BUTTON_START  wwwww_tag_wwwww__CC_FC676F
+#define C_BUTTON_END  wwwww_tag_wwwww__CC_FF9944
+
 #else
 
 #define BaseColor  wwwww_tag_wwwww__CC_FF892E
