94ac8a5adc66541b99cd1a6fb73b596d8aef84b5
Gan Yuanrong <372129081@qq.com>
2023-08-28 17:24:52 +0800

    生成framework

---
+++ GamaSDK_iOS_Integration/build/MWSDK_KR.xcframework/ios-arm64/MWSDK_KR.framework/Headers/LoginData.h
@@ -0,0 +1,45 @@
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginData : NSObject
+
+@property (nonatomic, copy) NSString *userId;
+@property (nonatomic, strong) NSString *cre3734Rememberatic3735;//===insert my property===
+
+@property (nonatomic, copy) NSString *timestamp;
+@property (nonatomic, strong) NSObject *heardom3736Loquithroughout3737;//===insert my property===
+
+@property (nonatomic, strong) NSString *thoughtoon3738Tribo3739;//===insert my property===
+
+@property (nonatomic, copy) NSString *accessToken;
+@property (nonatomic, assign) float gelent3740Eso3741;//===insert my property===
+
+@property (nonatomic, copy) NSString *thirdPlate;
+
+@property (nonatomic, assign) BOOL isBind;
+@property (nonatomic, assign) CGFloat spaceetic3742Sexagenfaction3743;//===insert my property===
+
+@property (nonatomic, strong) NSString *ramian3744Ethoatory3745;//===insert my property===
+
+@property (nonatomic, assign) float mention3746Worker3747;//===insert my property===
+
+@property (nonatomic, assign) BOOL isBindPhone;
+@property (nonatomic, strong) NSString *futureatic3748Quite3749;//===insert my property===
+
+
+@property (nonatomic, copy) NSString *loginType;
+
+@property (nonatomic, copy) NSString *telephone;
+@property (nonatomic, copy) NSString *loginId;
+@property (nonatomic, copy) NSString *sign;
+@property (nonatomic, strong) NSDictionary *dreamship3750End3751;//===insert my property===
+
+@property (nonatomic, assign) NSInteger strigic3752Miasmataheadfold3753;//===insert my property===
+ 
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GamaSDK_iOS_Integration/build/MWSDK_KR.xcframework/ios-arm64_x86_64-simulator/MWSDK_KR.framework/Headers/PayData.h
@@ -0,0 +1,30 @@
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PayData : NSObject
+
+@property (nonatomic, copy) NSString *orderId;
+@property (nonatomic, copy) NSString *transactionId;
+@property (nonatomic, strong) NSDictionary *gerice3754Answerty3755;//===insert my property===
+
+@property (nonatomic, assign) NSInteger largitive3756Institutionfy3757;//===insert my property===
+
+@property (nonatomic, assign) CGFloat vuls3758From3759;//===insert my property===
+
+@property (nonatomic, copy) NSString *productId;
+@property (nonatomic, copy) NSString *cpOrderId;
+@property (nonatomic, assign) CGFloat amount;
+@property (nonatomic, assign) int brotherade3760Ballattack3761;//===insert my property===
+
+@property (nonatomic, assign) int leadot3762Stigious3763;//===insert my property===
+ 
+
+@property (nonatomic, copy) NSString *timestamp;
+
+@end
+
+NS_ASSUME_NONNULL_END
